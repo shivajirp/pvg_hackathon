a9bc96ed024a933e3bf2c08588d394baba8802ca
Samarth Kamble <kamblesamarth04@gmail.com>
2025-03-22 11:02:17 +0530

    Intializa the server setup

---
+++ server/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+
+require("dotenv").config();
+
+export const app = express();
+
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
+});
+
+app.use(cookieParser());
