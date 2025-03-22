b98909db789302ba053d54c181f86fb909116334
shivajirp <shivajirp123@gmail.com>
2025-03-22 11:26:26 +0530

    mongodb connected

--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+// import dotenv from "dotenv";
+import { connectDB } from "./db/connectDB";
 
 require("dotenv").config();
 
 export const app = express();
 
+const PORT = process.env.PORT || 5000;
+
 app.use(express.json());
+app.use(cookieParser());
 
 app.use(cors({
   origin: "http://localhost:3000",
@@ -14,7 +19,7 @@ app.use(cors({
 }))
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server is running on http://localhost:${process.env.PORT}`);
+  connectDB();
+  console.log(`Server is running on http://localhost:${PORT} || PORT`);
 });
 
-app.use(cookieParser());
